b6ff9dc2f363de19dc05d22d8f51490f97362245
Eoin Roe <eoinroe@eoins-mbp.mynet>
2020-09-12 00:03:57 +0100

    switched to using dictionaries since the linked functions have a groups property which allows the compiler to make optimizations when using visible functions

--- visibleFunctions/Renderer.swift
+++ visibleFunctions/Renderer.swift
@@ -11,15 +11,54 @@ import MetalKit
 final class Renderer: NSObject {
     var metal: (device: MTLDevice, queue: MTLCommandQueue, library: MTLLibrary)
     
-    var compute: (pipeline: MTLComputePipelineState, table: MTLVisibleFunctionTable)
+    var gradientFunctions: [String] = ["purple_gradient", "turquoise_gradient", "yellow_gradient"]
     
-    var visibleFunctions: [String] = ["purple_gradient", "turquoise_gradient"]
+    var recursiveFunctions: [String] = ["recursive"]
+    
+    var functions = [String: [MTLFunction]]()
+        
+    var computePipelineState: MTLComputePipelineState
+    
+    var visibleFunctionTable: MTLVisibleFunctionTable
+    
+    // var functionTable: MTLVisibleFunctionTable
+    
+    var visibleFunctionGroups: [String: [String]] = ["gradients" : ["purple_gradient", "turquoise_gradient", "yellow_gradient"],
+                                                     "recursive" : ["test"]]
+    
     
     var index: UInt32 = 0
     
     init(view: MTKView) {
         self.metal = Renderer.setupMetal()
-        self.compute = Renderer.setupComputePipeline(device: metal.device, library: metal.library, kernelFunction: "visible", visibleFunctions: visibleFunctions)
+        
+        /*
+        
+        let functions: [MTLFunction] = self.visibleFunctions.map({
+            guard let function = metal.library.makeFunction(name: $0) else {
+                fatalError("Couldn't create the visible function \($0).")
+            }
+            return function
+        })
+ 
+        */
+        
+        // let visibleFunctions = gradientFunctions + recursiveFunctions
+        // let visibleFunctions = gradientFunctions.append(contentsOf: recursiveFunctions)
+        
+        for (group, functions) in visibleFunctionGroups {
+            self.functions[group] = Renderer.setupFunctions(library: metal.library, visibleFunctions: functions)
+        }
+        
+        // self.functions["gradients"] = Renderer.setupFunctions(library: metal.library, visibleFunctions: visibleFunctionGroups["gradients"]!)
+        // self.functions["test"] = Renderer.setupFunctions(library: metal.library, visibleFunctions: visibleFunctionGroups["test"]!)
+        
+        
+        self.computePipelineState = Renderer.setupComputePipeline(device: metal.device, library: metal.library, kernelFunction: "visible", functionGroups: functions)
+        
+        self.visibleFunctionTable = Renderer.setupFunctionTable(library: metal.library, pipeline: computePipelineState, name: "gradients", group: functions)
+        
+        // self.functionTable = Renderer.setupFunctionTable(library: metal.library, pipeline: computePipelineState, name: "test", group: test)
         
         // Sometimes you know a throwing function or method won’t, in fact, throw an error at runtime.
         // On those occasions, you can write try! before the expression to disable error propagation.
@@ -48,10 +87,7 @@ private extension Renderer {
         return (device, queue, library)
     }
     
-    static func setupComputePipeline(device: MTLDevice, library: MTLLibrary, kernelFunction: String, visibleFunctions: [String]) -> (pipeline: MTLComputePipelineState, table: MTLVisibleFunctionTable) {
-        guard let computeFunction = library.makeFunction(name: kernelFunction) else {
-            fatalError("Couldn't create the kernel function.")
-        }
+    static func setupFunctions(library: MTLLibrary, visibleFunctions: [String]) -> [MTLFunction] {
         
         let functions: [MTLFunction] = visibleFunctions.map({
             guard let function = library.makeFunction(name: $0) else {
@@ -59,13 +95,122 @@ private extension Renderer {
             }
             return function
         })
+        
+        return functions
+    }
+    
+    static func setupComputePipeline(device: MTLDevice, library: MTLLibrary, kernelFunction: String, groups: [String: [MTLFunction]]) -> (pipeline: MTLComputePipelineState, table: MTLVisibleFunctionTable) {
+        
+        guard let computeFunction = library.makeFunction(name: kernelFunction) else {
+            fatalError("Couldn't create the kernel function.")
+        }
+        
+        /*
+        
+        let functions: [MTLFunction] = visibleFunctions.map({
+            guard let function = library.makeFunction(name: $0) else {
+                fatalError("Couldn't create the visible function \($0).")
+            }
+            return function
+        })
+ 
+        */
+        
+        // let functions = groups.map { (group, function) in return function }
+        
+        // let test = Array(groups.values)
+        // groups.values.reduce([], +)
+            
+        // var functions: [MTLFunction] = groups.values.reduce([], +)
+        let functions: [MTLFunction] = Array(groups.values.joined())
+        
+        /*
+        for f in groups.values {
+            functions.append(contentsOf: f)
+        }
+         */
                         
         let linkedFunctions = MTLLinkedFunctions()
         linkedFunctions.functions = functions
+        linkedFunctions.groups = groups
         
         let descriptor = MTLComputePipelineDescriptor()
         descriptor.computeFunction = computeFunction
         descriptor.linkedFunctions = linkedFunctions
+        
+        // For recursion
+        descriptor.maxCallStackDepth = 3
+        
+        guard let pipeline = try? device.makeComputePipelineState(descriptor: descriptor,
+                                                                 options: [],
+                                                                 reflection: nil) else {
+            fatalError("Couldn't create the compute pipeline state.")
+        }
+        
+        
+        // Use the compute pipeline to create the visible function table
+        let vftDescriptor = MTLVisibleFunctionTableDescriptor()
+        vftDescriptor.functionCount = functions.count
+        
+        guard let functionTable = pipeline.makeVisibleFunctionTable(descriptor: vftDescriptor) else {
+            fatalError("Couldn't create the visible function table.")
+        }
+        
+        for index in 0..<functions.count {
+            let functionHandle = pipeline.functionHandle(function: functions[index])
+            functionTable.setFunction(functionHandle, index: index)
+        }
+        
+        return (pipeline, functionTable)
+    }
+    
+    static func setupComputePipeline(device: MTLDevice, library: MTLLibrary, kernelFunction: String, functionGroups: [String: [MTLFunction]]) -> MTLComputePipelineState {
+        
+        guard let computeFunction = library.makeFunction(name: kernelFunction) else {
+            fatalError("Couldn't create the kernel function.")
+        }
+        
+        let functions: [MTLFunction] = Array(functionGroups.values.joined())
+                        
+        let linkedFunctions = MTLLinkedFunctions()
+        linkedFunctions.functions = functions
+        linkedFunctions.groups = functionGroups
+        
+        let descriptor = MTLComputePipelineDescriptor()
+        descriptor.computeFunction = computeFunction
+        descriptor.linkedFunctions = linkedFunctions
+        
+        // For recursion
+        descriptor.maxCallStackDepth = 3
+        
+        guard let pipeline = try? device.makeComputePipelineState(descriptor: descriptor,
+                                                                 options: [],
+                                                                 reflection: nil) else {
+            fatalError("Couldn't create the compute pipeline state.")
+        }
+        
+        return pipeline
+    }
+    
+    /*
+    
+    static func setupComputePipeline(device: MTLDevice, library: MTLLibrary, kernelFunction: String, visibleFunctions: [MTLFunction]) -> MTLVisibleFunctionTable {
+        guard let computeFunction = library.makeFunction(name: kernelFunction) else {
+            fatalError("Couldn't create the kernel function.")
+        }
+                        
+        let linkedFunctions = MTLLinkedFunctions()
+        linkedFunctions.functions = visibleFunctions
+        // linkedFunctions.group
+        
+        let descriptor = MTLComputePipelineDescriptor()
+        descriptor.computeFunction = computeFunction
+        descriptor.linkedFunctions = linkedFunctions
+        
+        
+        
+        // For recursion
+        descriptor.maxCallStackDepth = 3
         
         guard let pipeline = try? device.makeComputePipelineState(descriptor: descriptor,
                                                                  options: [],
@@ -82,12 +227,37 @@ private extension Renderer {
             fatalError("Couldn't create the visible function table.")
         }
         
+        for index in 0..<visibleFunctions.count {
+            let functionHandle = pipeline.functionHandle(function: visibleFunctions[index])
+            functionTable.setFunction(functionHandle, index: index)
+        }
+        
+        return functionTable
+    }
+ 
+    */
+    
+    static func setupFunctionTable(library: MTLLibrary, pipeline: MTLComputePipelineState, name: String, group: [String: [MTLFunction]]) -> MTLVisibleFunctionTable {
+        guard let functions = group[name] else {
+            fatalError("The functions could not be found.")
+        }
+            
+        // Use the compute pipeline to create the visible function table
+        let vftDescriptor = MTLVisibleFunctionTableDescriptor()
+        vftDescriptor.functionCount = functions.count
+            
+        guard let functionTable = pipeline.makeVisibleFunctionTable(descriptor: vftDescriptor) else {
+            fatalError("Couldn't create the visible function table.")
+        }
+            
         for index in 0..<functions.count {
+            print(index)
+            
             let functionHandle = pipeline.functionHandle(function: functions[index])
             functionTable.setFunction(functionHandle, index: index)
         }
         
-        return (pipeline, functionTable)
+        return functionTable
     }
 }
 
@@ -103,16 +273,19 @@ extension Renderer: MTKViewDelegate {
             return
         }
         
+        print(index)
+        
         let computeEncoder = commandBuffer.makeComputeCommandEncoder()!
-        computeEncoder.setComputePipelineState(compute.pipeline)
+        computeEncoder.setComputePipelineState(computePipelineState)
         
         computeEncoder.setBytes(&index, length: MemoryLayout<UInt32>.stride, index: 0)
-        computeEncoder.setVisibleFunctionTable(compute.table, bufferIndex: 1)
+        computeEncoder.setVisibleFunctionTable(visibleFunctionTable, bufferIndex: 1)
+        // computeEncoder.setVisibleFunctionTable(functionTable, bufferIndex: 2)
         computeEncoder.setTexture(drawable.texture, index: 0)
         
         // https://developer.apple.com/documentation/metal/calculating_threadgroup_and_grid_sizes
-        let w = compute.pipeline.threadExecutionWidth
-        let h = compute.pipeline.maxTotalThreadsPerThreadgroup / w
+        let w = computePipelineState.threadExecutionWidth
+        let h = computePipelineState.maxTotalThreadsPerThreadgroup / w
         let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
         
         let threadsPerGrid = MTLSize(width: drawable.texture.width,
--- visibleFunctions/Shaders.metal
+++ visibleFunctions/Shaders.metal
@@ -20,14 +20,36 @@ float4 turquoise_gradient(float2 st)
     return float4(0.0, st.x, st.y, 1.0);
 }
 
+[[visible]]
+float4 yellow_gradient(float2 st)
+{
+    return float4(st.x, st.y, 0.0, 1.0);
+}
+
+[[visible]]
+float test(float r, unsigned int depth)
+{
+    if (depth <= 0) {
+        return r;
+    }
+    
+    return 0.5 * test(r, depth - 1);
+    // return 0.5;
+}
+
 using GradientFunction = float4(float2);
+using RecursiveFunction = float(float, unsigned int);
 
 kernel void visible(constant unsigned int &index [[buffer(0)]],
-                    visible_function_table<GradientFunction> functions [[buffer(1)]],
+                    visible_function_table<GradientFunction> gradient_functions [[buffer(1)]],
+                    // visible_function_table<RecursiveFunction> recursive_functions [[buffer(2)]],
                     texture2d<float, access::write> tex0,
                     uint2 tid [[thread_position_in_grid]])
 {
     float2 uv = (float2)tid / float2(tex0.get_width(), tex0.get_height());
+    tex0.write(gradient_functions[index](uv), tid);
     
-    tex0.write(functions[index](uv), tid);
+    // Expecting dark gray color...
+    // float r = recursive_functions[0](1.0f, 3);
+    // tex0.write(float4(float3(r), 1.0), tid);
 }
